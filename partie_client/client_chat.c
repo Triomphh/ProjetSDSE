db28d759e2e3e7872df419028e9b0d38f3368e24
Triomph <worker13prime@gmail.com>
2024-05-01 16:32:13 +0200

    Script de compilation locale de xTerm

--- partie_client/client_chat.c
+++ partie_client/client_chat.c
@@ -1,6 +1,6 @@
 /*
     cmd compilation : 
-        clear; gcc -o afficheur_message afficheur_message.c; gcc -o client client_chat.c ../fonctions/creerSocketTCP.c -lpthread; ./client localhost 4006
+        clear; gcc -o afficheur_message afficheur_message.c; gcc -o client client_chat.c ../fonctions/creerSocketTCP.c -lpthread; ./client localhost 4009
 
     Ressources : 
         PIPES : 
@@ -163,8 +163,8 @@ int main( int argc, char **argv )
         char str_fd[4];
         sprintf( str_fd, "%d", pipe_fd[0] );
 
-        // execl( "/usr/bin/xterm", "xterm", "-e", "./afficheur_message", str_fd, NULL );
-        execl( "/usr/bin/gnome-terminal", "gnome-terminal", "--", "./afficheur_message", str_fd, NULL );
+        execl( "/usr/bin/xterm", "xterm", "-e", "./afficheur_message", str_fd, NULL );
+        // execl( "/usr/bin/gnome-terminal", "gnome-terminal", "--", "./afficheur_message", str_fd, NULL );
         perror( "Erreur d'exécution du terminal 'Afficheur Message' : " );
         exit( EXIT_FAILURE );
     }
